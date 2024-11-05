f3d3391db620e6807b1c16ba476a3b01dd74f9e9
Safiye-Teymourzadeh <farnooshteymourzadeh@gmail.com>
2024-11-05 11:46:49 +0100

    deleted all the folders

--- not_important_code/galaxies_in_clusters.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-# # make test galaxies in some clusters
-# list_of_galaxies = pd.DataFrame()   # uber_id mstar RA dec redshift
-# list_of_clusters = pd.DataFrame()   # uber_id RA dec redshift
-#
-# # fucntion that goes to each galaxy and cluster to check the distance and their positions
